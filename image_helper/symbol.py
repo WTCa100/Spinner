03a2e7bc8e252033e26d22e885dc16878a86fd98
John Peter Bielawa <wtca100@gmail.com>
2026-08-05 00:36:15 +0200

    Added watermelon sprite.

--- image_helper/symbol.py
+++ image_helper/symbol.py
@@ -10,6 +10,7 @@ SYMBOLS = [
     Symbol("res/Bannana-Sprite.png", 0, "Bannana"),
     Symbol("res/Cherry-Sprite.png", 1, "Cherry"),
     Symbol("res/Dollar-Sprite.png", 2, "Dollar"),
+    Symbol("res/Watermelon-Sprite.png", 3, "Watermelon"),
     Symbol("res/Seven-Sprite.png", 7, "Seven"),
     Symbol("res/Apple-Sprite.png", 13, "Apple"),
 ]
