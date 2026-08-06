2b2dfdcae12f1e37df69aef0ea2b01ddc7afcac3
John Peter Bielawa <wtca100@gmail.com>
2026-08-06 13:52:33 +0200

    Added new symbol loading

--- image_helper/symbol.py
+++ image_helper/symbol.py
@@ -12,6 +12,10 @@ SYMBOLS = [
     Symbol("res/Dollar-Sprite.png", 2, "Dollar"),
     Symbol("res/Watermelon-Sprite.png", 3, "Watermelon"),
     Symbol("res/Bar-Sprite.png", 4, "Bar"),
+    Symbol("res/Lemon-Sprite.png", 5, "Lemon"),
+    Symbol("res/Orange-Sprite.png", 6, "Orange"),
     Symbol("res/Seven-Sprite.png", 7, "Seven"),
+    Symbol("res/Grapes-Sprite.png", 8, "Grapes"),
+    Symbol("res/Rose-Sprite.png", 9, "Rose"),
     Symbol("res/Apple-Sprite.png", 13, "Apple"),
 ]
